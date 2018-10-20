0de07edabeb5f1e4767cc398d452ee703ad727ca
LyleCheatham <hello@lylecheatham.com>
2018-10-19 23:47:07 -0400

    Boost doesn't allow pushing brace init lists on linux or in 1.62

--- host/src/PacketHandler.cpp
+++ host/src/PacketHandler.cpp
@@ -63,14 +63,14 @@ bool PacketHandler::send_packet(std::unique_ptr<Packet_Data> data) {
 
     if(!response.first) return false;
 
-    sent_list.push_back(
-        {
-            .serial_number = response.second->get_serial_number(),
-            .callback_void = nullptr,
-            .callback_data = nullptr,
-            .packet = std::move(response.second)
-        }
-    );
+    // TODO: Fix this
+
+//    send_entry to_back = {.serial_number = response.second->get_serial_number(),
+//                          .callback_void = nullptr,
+//                          .callback_data = nullptr,
+//                          .packet = std::move(response.second)};
+//
+//    sent_list.push_back(to_back);
 
     return true;
 }
